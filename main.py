7ea7b7d1299545c5b6389409f78c8959aa1b1451
Bin824 <zomberrylol@gmail.com>
2025-03-14 14:37:52 -0400

    .

--- main.py
+++ main.py
@@ -4,26 +4,61 @@ Email: bin.ni81@myhunter.cuny.edu
 Resources: None
 """
 
-import math
+def gcd(mmm, nnn):
+    """
+    Helper function that computes the GCD of two numbers.
+    """
+    while mmm % nnn != 0:
+        oldm = mmm
+        oldn = nnn
+        mmm = oldn
+        nnn = oldm % oldn
+    return nnn
 
 class Fraction:
     """
-    Initializes a Fraction object and simplifies it using GCD
+    The Fraction class from ThinkCS Chapter 18.
     """
-    def __init__(self, numerator, denominator):
-        if denominator == 0:
-            raise ValueError("Denominator cannot be zero.")
-        common = math.gcd(numerator, denominator)
-        self.numerator = numerator // common
-        self.denominator = denominator // common
+    def __init__(self, top, bottom):
+        """
+        Initializes a fraction object.
+        """
+        self.num = top
+        self.den = bottom
+        self.simplify()
 
     def __str__(self):
-        return f"{self.numerator}/{self.denominator}"
+        """
+        Makes a nice string of the fraction for printing.
+        """
+        return str(self.num) + "/" + str(self.den)
+
+    def simplify(self):
+        """
+        Simplifies (cancels common factors) of the fraction.
+        """
+        common = gcd(self.num, self.den)
+        self.num = self.num // common
+        self.den = self.den // common
+
+    def __add__(self, otherfraction):
+        """
+        Adds two fractions (overloads +) and returns simplified sum.
+        """
+        newnum = self.num * otherfraction.den + self.den * otherfraction.num
+        newden = self.den * otherfraction.den
+        return Fraction(newnum, newden)
 
     def __eq__(self, other):
-        return (self.numerator * other.denominator) == (self.denominator * other.numerator)
+        """
+        Checks if two fractions are equivalent.
+        """
+        return self.num * other.den == self.den * other.num
 
     def __mul__(self, other):
-        new_numerator = self.numerator * other.numerator
-        new_denominator = self.denominator * other.denominator
-        return Fraction(new_numerator, new_denominator)
+        """
+        Multiplies two fractions and returns the simplified product.
+        """
+        newnum = self.num * other.num
+        newden = self.den * other.den
+        return Fraction(newnum, newden)
